2a37d76c172581662b15928afa434501c6673cf5
NakaDaoLe <genesis@nakadaole.com>
2020-05-09 20:11:43 +0800

    get domains

--- utils/hepler.js
+++ utils/hepler.js
@@ -2,4 +2,12 @@ const namehash = require("eth-ens-namehash");
 const sha3 = require("web3-utils").sha3;
 const toBN = require("web3-utils").toBN;
 
-console.log(namehash.hash("migrated.ela"))
+const SALT = sha3("foo");
+
+//console.log(SALT)
+
+// node => namehash
+console.log(namehash.hash("ela"))
+
+// id => sha3
+console.log(sha3("deloplen"));
