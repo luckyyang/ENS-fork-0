2784984b143df4bccf7b77c9f46492da49a2275e
NakaDaoLe <genesis@nakadaole.com>
2020-05-11 18:18:14 +0800

    compile reverseReg

--- utils/hepler.js
+++ utils/hepler.js
@@ -7,7 +7,9 @@ const SALT = sha3("foo");
 //console.log(SALT)
 
 // node => namehash
-console.log(namehash.hash("ela"))
+console.log(namehash.hash("reverse"))
 
 // id => sha3
-console.log(sha3("deloplen"));
+ console.log(sha3("addr"));
+//0x0000000000000000000000000000000000000000000000000000000000000000
+//0x4ef059036DEB6131bde9ceBdA74A0A9E792c2889
